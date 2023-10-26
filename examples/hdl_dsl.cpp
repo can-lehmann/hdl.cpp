af7c5abe732abd4dda58511f78312ad65ff62be9
Can Lehmann <can.l@posteo.de>
2023-10-26 19:49:40 +0200

    Add operators and memories to DSL

--- examples/hdl_dsl.cpp
+++ examples/hdl_dsl.cpp
@@ -1,5 +1,6 @@
 // Copyright (c) 2023 Can Joshua Lehmann
 
+#include <iostream>
 #include "../hdl_dsl.hpp"
 
 int main() {
@@ -8,13 +9,35 @@ int main() {
   hdl::dsl::synth(module, [&](){
     using namespace hdl::dsl;
     
-    Input<U<1>> clock("clock");
-    Reg<U<32>> reg;
+    Input<Bool> clock("clock");
+    Reg<U<32>> iter;
+    Reg<U<32>> out;
+    Mem<U<32>, 10> mem;
     
-    on(clock, [&](){
-      reg = reg + 1;
+    on(clock, ${
+      when(iter < 9, ${
+        iter = iter + 1;
+      }, ${
+        iter = 0;
+      });
+      
+      mem.write(iter, iter << 1);
+      out = mem[iter];
     });
+    
+    
+    module.output("out", out.value());
   });
+  
+  hdl::sim::Simulation sim(module);
+  bool clock = false;
+  for (size_t timestep = 0; timestep < 100; timestep++) {
+    sim.update({hdl::BitString::from_bool(clock)});
+    for (size_t it = 0; it < module.outputs().size(); it++) {
+      std::cout << module.outputs()[it].name << " = " << sim.outputs()[it] << std::endl;
+    }
+    clock = !clock;
+  }
   
   return 0;
 }
--- hdl_dsl.hpp
+++ hdl_dsl.hpp
@@ -15,7 +15,7 @@ namespace hdl {
     private:
       Module* _module = nullptr;
       Value* _clock = nullptr;
-      std::vector<hdl::Value*> conditions;
+      std::vector<Value*> _conditions;
     public:
       GlobalContext() {}
       
@@ -31,6 +31,27 @@ namespace hdl {
           throw Error("No clock");
         }
         return _clock;
+      }
+      
+      Value* condition() {
+        Value* condition = module().constant(BitString::from_bool(true));
+        for (Value* cond : _conditions) {
+          condition = module().op(Op::Kind::And, {
+            condition, cond
+          });
+        }
+        return condition;
+      }
+      
+      void when(Value* cond,
+                const std::function<void()>& then,
+                const std::function<void()>& otherwise) {
+        _conditions.push_back(cond);
+        then();
+        _conditions.pop_back();
+        _conditions.push_back(_module->op(Op::Kind::Not, {cond}));
+        otherwise();
+        _conditions.pop_back();
       }
       
       void on(Value* clock, const std::function<void()>& body) {
@@ -57,9 +78,6 @@ namespace hdl {
       Module& _module;
       Value* _value = nullptr;
     protected:
-      Val(Module& module, Value* value):
-        _module(module), _value(value) {}
-      
       void expect_same_module(const Val<Width>& other) const {
         if (&_module != &other._module) {
           throw Error("Not same module");
@@ -69,6 +87,9 @@ namespace hdl {
       Val(): _module(global_context.module()) {
         _value = _module.constant(BitString(Width));
       }
+      
+      Val(Module& module, Value* value):
+        _module(module), _value(value) {}
       
       Module& module() const { return _module; }
       Value* value() const { return _value; }
@@ -89,11 +110,14 @@ namespace hdl {
       Reg(): T(global_context.module(), create_register()) {}
       Reg(const T& initial): T(initial.module(), create_register(initial)) {}
       
-      template <size_t Width>
-      Reg<T>& operator=(const Val<Width>& val) {
+      Reg<T>& operator=(const T& val) {
         hdl::Reg* reg = dynamic_cast<hdl::Reg*>(this->value());
         reg->clock = global_context.clock();
-        reg->next = val.value();
+        reg->next = global_context.module().op(Op::Kind::Select, {
+          global_context.condition(),
+          val.value(),
+          reg->next
+        });
         return *this;
       }
     };
@@ -109,6 +133,59 @@ namespace hdl {
       Input(const char* name): T(global_context.module(), create_input(name, T())) {}
     };
     
+    class Bool: public Val<1> {
+    private:
+      static Value* create_bool(bool value) {
+        return global_context.module().constant(BitString::from_bool(value));
+      }
+    public:
+      using Val<1>::Val;
+      using Val<1>::module;
+      using Val<1>::value;
+      
+      Bool(bool value): Val<1>(global_context.module(), create_bool(value)) {}
+      
+      template <class T>
+      T select(const T& then, const T& otherwise) {
+        Value* result = Val<1>::module().op(Op::Kind::Select, {
+          value(),
+          then.value(),
+          otherwise.value()
+        });
+        return T(Val<1>::module(), result);
+      }
+      
+      #define binop(op_name, kind, invert) \
+        Bool operator op_name(const Bool& other) const { \
+          Val<1>::expect_same_module(other); \
+          Value* result = module().op(Op::Kind::kind, {value(), other.value()}); \
+          if (invert) { \
+            result = module().op(Op::Kind::Not, {result}); \
+          } \
+          return Bool(module(), result); \
+        }
+      
+      binop(&, And, false);
+      binop(|, Or, false);
+      binop(&&, And, false);
+      binop(||, Or, false);
+      binop(^, Xor, false);
+      
+      binop(==, Eq, false);
+      binop(!=, Eq, true);
+      
+      #undef binop
+      
+      Bool operator!() const {
+        Value* result = module().op(Op::Kind::Not, {value()});
+        return Bool(module(), result);
+      }
+      
+      Bool operator~() const {
+        return !(*this);
+      }
+    };
+    
     template <size_t Width>
     class U: public Val<Width> {
     private:
@@ -118,22 +195,88 @@ namespace hdl {
       }
     public:
       using Val<Width>::Val;
-      // Why?
       using Val<Width>::module;
       using Val<Width>::value;
       
       U(uint64_t constant):
         Val<Width>(global_context.module(), create_constant(constant)) {}
       
-      U<Width> operator+(const U<Width>& other) const {
-        Val<Width>::expect_same_module(other);
-        Value* result = module().op(Op::Kind::Add, {value(), other.value()});
-        return U<Width>(module(), result);
+      #define binop(op_name, kind) \
+        U<Width> operator op_name(const U<Width>& other) const { \
+          Val<Width>::expect_same_module(other); \
+          Value* result = module().op(Op::Kind::kind, {value(), other.value()}); \
+          return U<Width>(module(), result); \
+        }
+      
+      binop(&, And)
+      binop(|, Or)
+      binop(^, Xor)
+      
+      binop(+, Add)
+      binop(-, Sub)
+      binop(<<, Shl)
+      binop(>>, ShrU)
+      
+      #undef binop
+      
+      #define cmp(op_name, kind, invert) \
+        Bool operator op_name(const U<Width>& other) const { \
+          Val<Width>::expect_same_module(other); \
+          Value* result = module().op(Op::Kind::kind, {value(), other.value()}); \
+          if (invert) { \
+            result = module().op(Op::Kind::Not, {result}); \
+          } \
+          return Bool(module(), result); \
+        }
+      
+      cmp(==, Eq, false)
+      cmp(!=, Eq, true)
+      cmp(<, LtU, false)
+      cmp(>=, LtU, true)
+      cmp(<=, LeU, false)
+      cmp(>, LeU, true)
+      
+      #undef cmp
+    };
+    
+    template <class T, size_t Size>
+    class Mem {
+    private:
+      Module& _module;
+      Memory* _memory = nullptr;
+      
+      template <size_t Width>
+      static Memory* create_memory(const Val<Width>&) {
+        return global_context.module().memory(Width, Size, nullptr);
+      }
+    public:
+      Mem(): _module(global_context.module()), _memory(create_memory(T())) {}
+      
+      template <size_t AddressWidth>
+      T operator[](const U<AddressWidth>& address) {
+        return T(_module, _memory->read(address.value()));
+      }
+      
+      template <size_t AddressWidth>
+      void write(const U<AddressWidth>& address, const T& value) {
+        _memory->clock = global_context.clock();
+        _memory->write(address.value(), global_context.condition(), value.value());
       }
     };
     
-    void on(const U<1>& clock, const std::function<void()>& body) {
+    void on(const Bool& clock, const std::function<void()>& body) {
       global_context.on(clock.value(), body);
+    }
+    
+    void when(const Bool& cond,
+              const std::function<void()>& then,
+              const std::function<void()>& otherwise) {
+      global_context.when(cond.value(), then, otherwise);
+    }
+    
+    void when(const Bool& cond,
+              const std::function<void()>& then) {
+      when(cond, then, [](){});
     }
     
     void synth(Module& module, const std::function<void()>& body) {
@@ -142,5 +285,7 @@ namespace hdl {
     
   }
 }
+
+#define $ [&]()
 
 #endif
